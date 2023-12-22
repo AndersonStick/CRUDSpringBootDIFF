1f8e7dcfa4953dae3da699ca02425ff0fa6a2d93
Anderson Barrera <andersonbar10@gmail.com>
2023-12-22 15:35:04 -0500

    Todas las rutas correctas

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.api.crud.controllers;
 
 import com.api.crud.models.UserModel;
 import com.api.crud.services.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -20,7 +22,29 @@ public class UserController {
     }
 
     @PostMapping
-    public UserModel saveUser(@RequestBody){
-        return 0;
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public UserModel updateUserById(@RequestBody UserModel request, @PathVariable("id") Long id){
+        return this.userService.updateById(request, id);
+    }
+
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUserById(@PathVariable("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+
+        if (ok){
+            return "User with id " + id + " deleted!";
+        } else {
+            return "Error, we have a problem deleting user with id: " + id;
+        }
     }
 }
